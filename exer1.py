93ae45d4578978dd07f0cb949f2aca6d72c18d76
raulcosta2 <142070519+raulcosta2@users.noreply.github.com>
2023-09-01 16:50:32 -0300

    Update exer1.py

--- exer1.py
+++ exer1.py
@@ -1,4 +1,4 @@
 def substituir_vogal():
     frase = input("digite uma frase")
-    return frase.replace('a','*').replace('e','*').replace('i','*').replace('o','*').replace('u','*')
+    return frase.replace('a','*').replace('e','*').replace('i','*').replace('o','*').replace('u','*').replace('A','*').replace('E','*').replace('I','*').replace('O','*').replace('U','*')
 print(substituir_vogal())
